b194d285aca29f9edcae028db80b3dee506e6244
Eric Larson <eclarson@usr105-84.perunanet.smu.edu>
2015-02-26 12:12:35 -0600

    Initial Commit

---
+++ LookinLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LookinLive
+//
+//  Created by Eric Larson on 2/26/15.
+//  Copyright (c) 2015 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
